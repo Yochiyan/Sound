23e90c272fc433a93ea6d6b4479b2edb90a0c308
Swift <Swift>
2021-04-01 16:12:09 +0900

    サウンドApp(1)

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    //SoundFile read
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func drumtap() {
+        drumSoundPlayer.currentTime = 0
+        drumSoundPlayer.play()
+    
+    }
 }
 
