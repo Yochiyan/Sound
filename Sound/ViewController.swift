600b0d1f14ce5da30bf6dea2722354f3720d0a97
Swift <Swift>
2021-04-02 11:58:02 +0900

    ピアノとギター追加。エラー1件発生

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -9,15 +9,27 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    //drum
     @IBOutlet var drumButton: UIButton!
+    //piano
+    @IBOutlet var pianoButton: UIButton!
+    //guitar
+    @IBOutlet var guitarButton: UIButton!
     
-    //SoundFile read
+    //SoundFile drum
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    
+    //SoundFile piano
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "pianoSound")! .data)
+    
+    //SoundFile guitar
+    let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "guitarSound")! .data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //drumButton
     @IBAction func TouchDownDrumdrumtap() {
         drumButton.setImage(UIImage(named: "drumPlayingImage"),for: .normal)
         drumSoundPlayer.currentTime = 0
@@ -25,6 +37,23 @@ class ViewController: UIViewController {
 }
     @IBAction func touchUpDrumButtn() {
         drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
+ //pianoButton
+    @IBAction func touchDownPianotap() {
+        pianoButton.setImage(UIImage(named:"pianoPlayingImage"), for: .normal)
+        pianoSoundPlayer.currentTime = 0
+        pianoSoundPlayer.play()
+    }
+    @IBAction func touchUPpianoButtn() {
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+}//guitarButton
+    @IBAction func TouchDownGuitartap() {
+        guitarButton.setImage(UIImage(named: "guitarPlayingImage"),for: .normal)
+        guitarSoundPlayer.currentTime = 0
+        guitarSoundPlayer.play()
+}
+    @IBAction func touchUpGuitarButtn() {
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
     }
 }
 
